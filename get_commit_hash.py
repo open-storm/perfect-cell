fae562e0b4b51824bddee4908d4938bfd1e0ef4a
Matt Bartos <mdbartos@umich.edu>
2017-06-26 22:56:17 -0400

    Improved behavior of get_commit_hash

--- get_commit_hash.py
+++ get_commit_hash.py
@@ -1,43 +1,71 @@
 import os
 import sys
+import getopt
 
-if __file__:
+try:
     script_path = os.path.dirname(os.path.abspath(__file__))
-else:
-    script_path = os.get_cwd()
+except:
+    script_path = os.getcwd()
 
 log_path_relative = ['.git','logs', 'HEAD']
 header_path_relative = ['perfect-cell.cydsn', 'commit.h']
 
 log_path = os.path.join(script_path, *log_path_relative)
 header_path = os.path.join(script_path, *header_path_relative)
+ignore = False
 
-def get_last_commit_info(log_path):
+def get_last_commit_info(log_path, ignore=False):
+    """
+    Get info for last commit from git logs
+
+    log_path : Path to read log
+    ignore : If True, do not read log. Writes default values to header file.
+    """
+
     commit_info = {}
-    with open(log_path, 'r') as head:
-        lines = head.readlines()
-    info, message = lines[-1].split('\t')
-    prev_commit, current_commit, author, email, timestamp, tz = info.split()
+
+    # Default commit info
+    prev_commit = 40*'0'
+    current_commit = 40*'0'
+    if os.path.exists(log_path):
+        with open(log_path, 'r') as head:
+            lines = head.readlines()
+        info, message = lines[-1].split('\t')
+        print(info)
+        prev_commit, current_commit = info.split()[:2]
+    else:
+        if not ignore:
+            raise OSError('''Git log file not found. Please clone repository
+                          using git:\ngit clone
+                          https://github.com/open-storm/perfect-cell\n
+                          or use the --ignore flag''')
     commit_info['PREVIOUS_COMMIT'] = prev_commit
     commit_info['CURRENT_COMMIT'] = current_commit
-    commit_info['AUTHOR'] = author
-    commit_info['EMAIL'] = email
-    commit_info['COMMIT_TIME'] = timestamp
-    commit_info['COMMIT_TZ'] = tz
-    commit_info['COMMIT_MESSAGE'] = message.strip()
     return commit_info
 
 def write_commit_header(header_path, commit_info):
+    """
+    Write current git commit info to header file
+
+    header_path : Path to write header file
+    commit_info : Dict containing header info
+    """
+
     with open(header_path, 'w') as header:
         for name, value in commit_info.items():
             header.write("#define {0} \"{1}\"\n".format(name, value))
     return True
 
 if __name__ == "__main__":
-    cmd_line_args = sys.argv
-    if len(cmd_line_args) > 1:
-    	log_path = cmd_line_args[1]
-    if len(cmd_line_args) > 2:
-    	header_path = cmd_line_args[2]
-    commit_info = get_last_commit_info(log_path)
+    cmd_line_args = sys.argv[1:]
+    opts, remainder = getopt.getopt(cmd_line_args, 's:d:i', ['source=', 'dest=',
+                                                             'ignore'])
+    for opt, arg in opts:
+        if opt in ('-s', '--source'):
+            log_path = arg
+        if opt in ('-d', '--dest'):
+            header_path = arg
+        if opt in ('-i', '--ignore'):
+            ignore = True
+    commit_info = get_last_commit_info(log_path, ignore=ignore)
     write_commit_header(header_path, commit_info)
