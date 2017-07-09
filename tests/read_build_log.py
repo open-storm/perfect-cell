bbef37579b4c363792132654b037729775002418
Matt Bartos <mdbartos@umich.edu>
2017-07-09 06:25:32 -0400

    Attempt to fix logging

--- tests/read_build_log.py
+++ tests/read_build_log.py
@@ -5,13 +5,15 @@ from systemd import journal
 current_time = datetime.datetime.utcnow()
 
 if __name__ == "__main__":
-    build_time = sys.argv[1]
-    build_time = datetime.datetime.strptime(build_time, "%Y-%m-%d %H:%M:%S")
+    build_date = sys.argv[1]
+    build_time = sys.argv[2]
+    build_timestamp = datetime.datetime.strptime(build_date + ' ' + build_time,
+                                            "%Y-%m-%d %H:%M:%S")
     with journal.Reader() as j:
         j.this_boot()
         j.log_level(journal.LOG_INFO)
         j.add_match(_SYSTEMD_UNIT="influxdb.service")
-        j.seek_realtime(build_time)
+        j.seek_realtime(build_timestamp)
         entry = j.get_next()
         while entry:
             print(entry['MESSAGE'])
