3f2b5d57efdd1d4ad30274389673a826fad7e736
Matt Bartos <mdbartos@umich.edu>
2017-09-20 15:28:04 -0400

    Updated wq

--- perfect-cell.cydsn/atlas_wq_sensor.h
+++ perfect-cell.cydsn/atlas_wq_sensor.h
@@ -5,8 +5,6 @@
  * @version TODO
  * @date 2017-06-19
  */
-#ifndef ATLAS_WQ_SENSOR_H
-#define ATLAS_WQ_SENSOR_H
 #include "project.h"
 
 #define CONDUCTIVITY 100
@@ -14,10 +12,6 @@
 #define DO 97
 #define ORP 98
 #define PH 99
-
-#define ATLAS_MAX_ITER 100
-
-#define ATLAS_MAX_ITER 100
 
 struct{
     float ec;
@@ -36,13 +30,13 @@ struct{
 int atlas_sensor_sleep(uint8 sensor_address);
 
 /**
- * @brief Calibrate Atlas sensor using single-point method
+ * @brief Wake Atlas I2C sensor from sleep mode
  *
  * @param sensor_address I2C slave address of sensor
  *
  * @return 1 on success
  */
-int atlas_sensor_calibrate(uint8 sensor_address);
+int atlas_sensor_wake(uint8 sensor_address);
 
 /**
  * @brief Take a single reading from Atlas sensor in I2C mode.
@@ -77,5 +71,4 @@ uint8 atlas_take_con_reading(con_reading *reading);
  */
 uint8 zip_atlas_wq(char *labels[], float readings[], uint8 *array_ix, uint8 max_size);
 
-#endif
 // Additional functionality
