d00d2c60d8d0cc9bc26b10ed881197caf8cc1025
Matt Bartos <mdbartos@umich.edu>
2018-09-30 22:32:32 -0400

    Update tipping bucket. Disable debouncer due to dependence on clocks and add delay as interim fix to account for bouncing of tipping mechanism.

--- perfect-cell.cydsn/optical_rain.c
+++ perfect-cell.cydsn/optical_rain.c
@@ -8,13 +8,33 @@
 
 #include "optical_rain.h"
 
-uint8 count = 0;
+uint8  event = 0; // Keep track of each time the rain gage triggers an interrupt
+uint16 count = 0; // Number of "tips" 
+
 // prototype modem interrupt
 CY_ISR_PROTO(isr_optical_rain);
 
 CY_ISR(isr_optical_rain){
-    // hold the next char in the rx register as a temporary variable
-    count++;  
+    // This interrupt is triggered by two events when using a tipping bucket
+    // 1) Rising edge caused by an actual tip 
+    // 2) A series of rising edges caused by bouncing as the tipping mechanism settles
+    
+    // Increment each time an interrupt is triggered by the rain gage
+    event++;   
+    
+    // Delay 500 microseconds to allow bouncing signal to pass
+    // /!\ NOTE: This is not good practice and should either be fixed 
+    //     by keeping track of the time or adding an RC circuit in order
+    //     to prevent blocking other processes
+    CyDelayUs(500u);
+  
+    // Update the number of "tips". This code double counts each tip, so 
+    // use this if-statement to increment the actual number of tips once every 2 times
+    if (event%2 == 0) {
+        event = 0;
+        count++;
+        //LED_Write(!LED_Read());
+    }
 }
 
 uint8 optical_rain_get_count() {
